d3cd6f95bd3ce4cb9b143cff72eee7eda3f6517f
theMadhavR <madhavr003z@gmail.com>
2025-07-17 10:14:05 +0530

    Update weather.py

--- SmartNavFish-Fishing Route Optimizer/app/models/weather.py
+++ SmartNavFish-Fishing Route Optimizer/app/models/weather.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, JSON
 from sqlalchemy.sql import func
 from app.database import Base
 
